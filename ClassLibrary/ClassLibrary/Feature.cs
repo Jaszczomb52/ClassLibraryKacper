4cf26c3dbdbe4fb5d00baeca40d93be99f0be204
Jaszczomb52 <cmdr.blackburn@gmail.com>
2021-07-29 09:45:44 +0200

    poczatkowy stuff

---
+++ ClassLibrary/ClassLibrary/Feature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Feature
+    {
+        string[] help { get; set; }
+        public Feature(string[] help)
+        {
+            this.help = help;
+        }
+
+        public void GetHelp()
+        {
+            foreach(string line in help)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
